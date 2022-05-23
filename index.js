16014ae6e60418d78d858967a8222ce75f8bfe4f
Aruna Naidu <arnaidu@accurate.com>
2022-05-23 14:54:56 +0530

    PLAPI-5881: code updated

--- index.js
+++ index.js
@@ -11,15 +11,33 @@ function onLoad2() {
 
 }
 function uploadSelectedDocument(element,t){
+
     var selectedText = t.options[t.selectedIndex].innerHTML;
     var uploadElement =  document.getElementById(element);
     var displaySelect = document.getElementById('subSelect');
-    var onCheck = document.getElementById('onCheck1');
+    var onCheck1 = document.getElementById('onCheck1');
+    var onCheck = document.getElementById('onCheck');
     if(!(t.options[t.selectedIndex].value=="NA")){
         uploadElement.style.display="flex";
         uploadElement.childNodes[0].textContent = selectedText;
-        onCheck.style.display="inline";
+        onCheck1.style.display="inline";
         displaySelect.style.display= 'inline-block';
+    } else if(t.options[t.selectedIndex].value=="NA"){
+
+       alert("Please submit any one of the given documents");
+        t.style.display = 'none';
+        onCheck.style.display = 'none';
+        uploadElement.style.display="none";
+        onCheck1.style.display="inline";
+        displaySelect.style.display= 'inline-block';
+    }
+}
+function uploadSelectedDoc(element,y){
+    var selectedText = y.options[y.selectedIndex].innerHTML;
+    var uploadElement =  document.getElementById(element);
+    if(y.options[y.selectedIndex].value){
+        uploadElement.style.display="flex";
+        uploadElement.childNodes[0].textContent = selectedText;
     } else{
        alert("Please submit any one of the given documents");
         uploadElement.style.display="none";
@@ -29,12 +47,10 @@ function onFileUpload(val) {
     alert("File Uploaded Successfully" + val);
     document.getElementById("onCheck").checked = true;
     document.getElementById("uploadDoc").style.display="none";
-  }
-function onSubmitCheck(x){
-    var onUpload = x.type.uploaded.value;
-    let fileUploaded = document.getElementById("onCheck");
-    if(onUpload == true){
-        alert("file uploaded Successfully");
-        fileUploaded.checked = true;
-    }
 }
+function onFileUpload1(val) {
+    alert("File Uploaded Successfully" + val);
+    document.getElementById("onCheck1").checked = true;
+    document.getElementById("uploadDoc1").style.display="none";
+  }
+
