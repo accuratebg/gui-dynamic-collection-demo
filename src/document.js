1905b649cd7a885c87c297098c51770f01abd35b
Aruna <aruna.n.pro@gmail.com>
2022-06-08 12:56:25 +0530

    integatind document collection with UI

--- src/document.js
+++ src/document.js
@@ -1,60 +1,179 @@
 //POST
-function onRadioUk(){
+function onRadioUk() {
   if (document.getElementById("UK").checked) {
-    document.getElementById('onBtn').style.display="block";
+    document.getElementById("onBtn").style.display = "block";
   }
 }
-function onLoad() {
+function onRadioNonUK() {
+  var selectOpt1 = document.getElementById("dropDownSection");
+  selectOpt1.classList.remove("show");
+  selectOpt1.classList.add("hide");
+}
+function onFileUpload(val) {
+  document.getElementById("onCheck").checked = true;
+  document.getElementById("onBtn").style.display = "block";
+}
+function onFileUpload1(val) {
+  document.getElementById("onCheck1").checked = true;
+  document.getElementById("onBtn").style.display = "block";
+}
+
+function onSubmit() {
   let val = document.getElementById("UK").value;
-  console.log(val);
-  let obj = { step: "CHOOSE_NATIONALITY", sequence:"1", values:[""+val+""]};
-  console.log(typeof obj);
-  let strObj= JSON.stringify(obj);
-  console.log(strObj);
-  console.log(typeof strObj);
-  let form = new FormData();
-  form.append(
-    "documentSessionStepRequest",
-    strObj
-  );
-  
-  let settings = {
-    url: "http://localhost:8090/v1/document-session/62971d4e8ff5147c524acf0e/step/save",
+  let dropdownSectionStyle = document.getElementById("dropDownSection");
+  let sectionStyle = window
+    .getComputedStyle(dropdownSectionStyle, null)
+    .getPropertyValue("display");
+  let subDropdown = document.getElementById("subDropDown");
+  let sectionSubStyle = window
+    .getComputedStyle(subDropdown, null)
+    .getPropertyValue("display");
+
+  if (
+    document.getElementById("UK").checked &&
+    document.getElementById("onBtn").style.display == "block" &&
+    sectionStyle == "none"
+  ) {
+    let sequence = "1";
+    formDataValues(sequence, val);
+    document.getElementById("onBtn").style.display = "none";
+  } else if (
+    document.getElementById("UK").checked &&
+    document.getElementById("onCheck").checked == true &&
+    sectionStyle == "block" &&
+    sectionSubStyle == "none"
+  ) {
+    let sequence = "2";
+    formDataValues(sequence);
+    document.getElementById("onBtn").style.display = "none";
+  } else if (
+    document.getElementById("UK").checked &&
+    sectionStyle == "block" &&
+    sectionSubStyle == "block" &&
+    document.getElementById("onCheck1").checked == true
+  ) {
+    let sequence = "3";
+    formDataValues(sequence);
+  } else {
+    let sequence = "4";
+    formDataValues(sequence);
+  }
+}
+
+function getPayloadTemplate() {
+  let payload = {
+    url: "http://localhost:8090/v1/document-session/62a04c64ffca4d56ae3ee496/step/save",
     method: "POST",
+    dataType: "json",
     timeout: 0,
     processData: false,
     mimeType: "multipart/form-data",
     contentType: false,
-    data: form,
+    data: "",
   };
 
-  $.ajax(settings).done(function(response) {
-    console.log(response);
-  });
+  return payload;
 }
 
+let formDataValues = async (sequence, val = null) => {
+  switch (sequence) {
+    case "1":
+      let obj = {
+        step: "CHOOSE_NATIONALITY",
+        sequence: "1",
+        values: ["" + val + ""],
+      };
+      let strObj = JSON.stringify(obj);
 
-function onLoad2() {
-  var selectOpt1 = document.getElementById("selectOption");
-  selectOpt1.classList.remove("show");
-  selectOpt1.classList.add("hide");
-}
+      let form = new FormData();
+      form.append("documentSessionStepRequest", strObj);
+      let payloadData = getPayloadTemplate();
+      payloadData.data = form;
+      await $.ajax(payloadData).done(function (response) {
+        response.nextStep.options.forEach((option) => {
+          var selectOpt1 = document.getElementById("dropDownSection");
+          selectOpt1.classList.remove("hide");
+          selectOpt1.classList.add("show");
+          let dropDownOpt = document.getElementById("dropdownOpt1");
+          const newOption = document.createElement("option");
+          newOption.value = option.value;
+          newOption.text = option.displayLabel;
+          dropDownOpt.appendChild(newOption);
+        });
+      });
+      break;
+
+    case "2":
+      var onCheck1 = document.getElementById("onCheck1");
+      var displaySelect = document.getElementById("dropdownOpt2");
+      let obj1 = {
+        step: "GROUP1_DOCUMENT_SELECTION",
+        sequence: "2",
+        values: ["4"],
+        metadata: { documentType: "OTHR" },
+      };
+      let strObj1 = JSON.stringify(obj1);
+      let formData = new FormData();
+      formData.append("file", uploaded.files[0]);
+      formData.append("documentSessionStepRequest", strObj1);
+      let payloadData1 = getPayloadTemplate();
+      payloadData1.data = formData;
+      await $.ajax(payloadData1).done(function (response) {
+        response.nextStep.options.forEach((option) => {
+          const newOption = document.createElement("option");
+          newOption.value = option.value;
+          newOption.text = option.displayLabel;
+          displaySelect.appendChild(newOption);
+        });
+
+        alert("File Uploaded Successfully");
+        document.getElementById("uploadDoc").style.display = "none";
+        document.getElementById("subDropDown").style.display = "block";
+        onCheck1.style.display = "inline";
+        displaySelect.style.display = "inline-block";
+      });
+      break;
+    case "3":
+      let obj2 = {
+        step: "GROUP1_OR_GROUP2a_OR_GROUP2b_SELECTION",
+        sequence: "3",
+        values: ["2"],
+        metadata: { documentType: "OTHR" },
+      };
+      let strObj2 = JSON.stringify(obj2);
+      let formData1 = new FormData();
+      formData1.append("file", uploaded1.files[0]);
+      formData1.append("documentSessionStepRequest", strObj2);
+      let payloadData2 = getPayloadTemplate();
+      payloadData2.data = formData1;
+      $.ajax(payloadData2).done(function (response) {
+        console.log(response);
+      });
+      alert("File Uploaded Successfully");
+      document.getElementById("uploadDoc1").style.display = "none";
+      document.getElementById("onCheck1").checked = true;
+      break;
+    case "4":
+      console.log("Invalid End Point");
+      break;
+  }
+};
+
 function uploadSelectedDocument(element, t) {
   var selectedText = t.options[t.selectedIndex].innerHTML;
   var uploadElement = document.getElementById(element);
-  var displaySelect = document.getElementById("subSelect");
+  var displaySelect = document.getElementById("dropdownOpt2");
   var onCheck1 = document.getElementById("onCheck1");
   var onCheck = document.getElementById("onCheck");
   if (!(t.options[t.selectedIndex].value == "NA")) {
     uploadElement.style.display = "flex";
     uploadElement.childNodes[0].textContent = selectedText;
-    onCheck1.style.display = "inline";
-    displaySelect.style.display = "inline-block";
   } else if (t.options[t.selectedIndex].value == "NA") {
     alert("Please submit any one of the given documents");
     t.style.display = "none";
     onCheck.style.display = "none";
     uploadElement.style.display = "none";
+    document.getElementById("subDropDown").style.display = "block";
     onCheck1.style.display = "inline";
     displaySelect.style.display = "inline-block";
   }
@@ -69,14 +188,4 @@ function uploadSelectedDoc(element, y) {
     alert("Please submit any one of the given documents");
     uploadElement.style.display = "none";
   }
-}
-function onFileUpload(val) {
-  alert("File Uploaded Successfully" + val);
-  document.getElementById("onCheck").checked = true;
-  document.getElementById("uploadDoc").style.display = "none";
-}
-function onFileUpload1(val) {
-  alert("File Uploaded Successfully" + val);
-  document.getElementById("onCheck1").checked = true;
-  document.getElementById("uploadDoc1").style.display = "none";
 }
